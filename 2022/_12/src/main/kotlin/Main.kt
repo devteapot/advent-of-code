9b0fe5cd85f10fa23cc4550e4b2b3da0568d7cc0
Diego Carlino <37153329+devteapot@users.noreply.github.com>
2022-12-12 14:27:03 +0100

    12

--- 2022/_12/src/main/kotlin/Main.kt
+++ 2022/_12/src/main/kotlin/Main.kt
@@ -3,29 +3,33 @@ import kotlin.math.floor
 import kotlin.streams.toList
 
 fun part1(inputPath: String): Int {
-    fun List<List<Int>>.search(from: Pair<Int, Int>, to: Pair<Int, Int>, visited: Set<Pair<Int, Int>> = emptySet(), step: Int = 0): Int {
-        if (from == to) return step
+    fun List<List<Int>>.search(from: Pair<Int, Int>, to: Pair<Int, Int>): Int {
+        var nodes = setOf(from)
+        val visited = mutableSetOf<Pair<Int, Int>>()
+        var count = 0
 
-        return listOf(
-            (from.first - 1 to from.second) to getOrNull(from.second)?.getOrNull(from.first - 1),
-            (from.first + 1 to from.second) to getOrNull(from.second)?.getOrNull(from.first + 1),
-            (from.first to from.second - 1) to getOrNull(from.second - 1)?.getOrNull(from.first),
-            (from.first to from.second + 1) to getOrNull(from.second + 1)?.getOrNull(from.first),
-        )
-            .asSequence()
-            .filter { it.second != null }
-            .filter {
-                getOrNull(from.second)?.getOrNull(from.first)?.let { current ->
-                    it.second!! <= current + 1
-                } ?: false
-            }
-            .map { it.first }
-            .filter { it !in visited }
-            .map { candidate ->
-                search(candidate, to, visited + from, step + 1)
-            }
-            .filter { it > 0 }
-            .minOrNull() ?: -1
+        while (to !in nodes) {
+            visited.addAll(nodes)
+            nodes = nodes
+                .flatMap { current ->
+                    listOf(
+                        current.first - 1 to current.second,
+                        current.first + 1 to current.second,
+                        current.first to current.second - 1,
+                        current.first to current.second + 1,
+                    )
+                        .filter { candidate ->
+                            (getOrNull(candidate.second)
+                                ?.getOrNull(candidate.first)
+                                ?.let { it <= this[current.second][current.first] + 1 }
+                                ?: false) && candidate !in visited
+                        }
+                }
+                .toSet()
+            count++
+        }
+
+        return count
     }
 
     return File(inputPath)
@@ -39,11 +43,52 @@ fun part1(inputPath: String): Int {
         }
 }
 
-fun part2(inputPath: String) =
-    File(inputPath)
-        .let { "TODO" }
+fun part2(inputPath: String): Int {
+    fun List<List<Int>>.search(from: Set<Pair<Int, Int>>, to: Pair<Int, Int>): Int {
+        var nodes = from
+        val visited = mutableSetOf<Pair<Int, Int>>()
+        var count = 0
+
+        while (to !in nodes) {
+            visited.addAll(nodes)
+            nodes = nodes
+                .flatMap { current ->
+                    listOf(
+                        current.first - 1 to current.second,
+                        current.first + 1 to current.second,
+                        current.first to current.second - 1,
+                        current.first to current.second + 1,
+                    )
+                        .filter { candidate ->
+                            (getOrNull(candidate.second)
+                                ?.getOrNull(candidate.first)
+                                ?.let { it <= this[current.second][current.first] + 1 }
+                                ?: false) && candidate !in visited
+                        }
+                }
+                .toSet()
+            count++
+        }
+
+        return count
+    }
+
+    return File(inputPath)
+        .readText()
+        .let { input ->
+            val lines = input.replace("S", "a").lines().map { it.chars().toList() }
+            val start = lines
+                .flatMapIndexed { y, line ->
+                    line.mapIndexedNotNull { x, c -> if (c == 'a'.code) x to y else null }
+                }
+                .toSet()
+            val end = floor(input.indexOf("E").toFloat() / lines[0].size).toInt().let { lines[it].indexOf('E'.code) to it }
+
+            input.replace("S", "a").replace("E", "z").lines().map { it.chars().toList() }.search(start, end)
+        }
+}
 
 fun main() {
     println("part 1: ${part1("test.in")}")
-    println("part 2: ${part2("example.in")}")
+    println("part 2: ${part2("test.in")}")
 }
